d6cf25c4290a01e9a569cee23133311d3860d559
LiliMeng <lilimeng1103@gmail.com>
2015-04-30 11:32:30 -0700

    Update Binary_Tree_Level_Order_Traversal.cpp

--- Binary_Tree_Level_Order_Traversal.cpp
+++ Binary_Tree_Level_Order_Traversal.cpp
@@ -31,19 +31,19 @@ public:
         //push every level of the tree into the result continuously as long as there is a TreeNode in the queue
         while(Q.size()!=0)
         {
-            TreeNode* newRoot = Q.front();    //Update the new root continuously by pushing the TreeNode* into and out of the queue
-            level.push_back(newRoot->val);
+            TreeNode* currentRoot = Q.front();    //Update the new root continuously by pushing the TreeNode* into and out of the queue
+            level.push_back(currentRoot->val);
             visitedCount++;
             Q.pop();    //pop out of the current root
 
-            if(newRoot->left!=NULL)
+            if(currentRoot->left!=NULL)
             {
-                Q.push(newRoot->left);
+                Q.push(currentRoot->left);
                 nextLevelCount++;
             }
-            if(newRoot->right!=NULL)
+            if(currentRoot->right!=NULL)
             {
-                Q.push(newRoot->right);
+                Q.push(currentRoot->right);
                 nextLevelCount++;
             }
             if(visitedCount==currentLevelCount)
@@ -59,39 +59,3 @@ public:
          return result;
 }
 };
-
-int main()
-{
-    TreeNode* n1 = new TreeNode(3);
-    TreeNode* n2 = new TreeNode(9);
-    TreeNode* n3 = new TreeNode(20);
-    TreeNode* n4 = new TreeNode(15);
-    TreeNode* n5 = new TreeNode(7);
-
-    n1->left=n2;
-    n1->right=n3;
-    n2->left=NULL;
-    n2->right=NULL;
-    n3->left=n4;
-    n3->right=n5;
-
-    n4->left=NULL;
-    n4->right=NULL;
-
-    n5->left=NULL;
-    n5->right=NULL;
-
-    Solution s;
-    vector<vector<int> > LO;
-    LO=s.levelOrder(n1);
-
-    for(auto ite=LO.begin();ite!=LO.end();ite++)
-    {
-        auto a=*ite;
-        for(auto i=a.begin();i!=a.end();i++)
-        {
-        cout<<*i<<endl;
-        }
-    }
-    return 0;
-}
