ea0aa938c81341d9ac743ee1a887442fc5e6f655
LiliMeng <lilimeng1103@gmail.com>
2015-05-12 11:13:40 -0700

    Update Find_Peak_Element.cpp

--- Find_Peak_Element.cpp
+++ Find_Peak_Element.cpp
@@ -3,6 +3,12 @@
 
 using namespace std;
 
+/*For this peak finding, the tricky part is
+
+if(nums[0]>nums[1])  return 0; 
+if(nums[nums.size()-1]>nums[nums.size()-2]) return nums.size()-1; 
+under the assumption that (nums[i]>nums[i+1] and nums[i]>nums[i-1]) could not be satisfied. */
+
 
 class Solution {
 public:
