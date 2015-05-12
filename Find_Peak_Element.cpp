08da0fc9fdd7a7c23f482f7142df29175ef54a8f
LiliMeng <lilimeng1103@gmail.com>
2015-05-12 11:08:25 -0700

    Create Find_Peak_Element.cpp

---
+++ Find_Peak_Element.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+
+    int findPeakElement(vector<int>& nums) {
+
+    for(int i=0; i<nums.size();i++)
+    {
+
+        if(nums[i]>nums[i+1] and nums[i]>nums[i-1])
+        return i;
+        else if(nums[0]>nums[1])
+        return 0;
+        else if(nums[nums.size()-1]>nums[nums.size()-2])
+        return nums.size()-1;
+
+    }
+
+    }
+};
+
+
+int main(){
+
+    vector<int> v={1,2,3,4,5,3,2};
+    Solution s;
+    cout<<s.findPeakElement(v);
+    return 0;
+}
