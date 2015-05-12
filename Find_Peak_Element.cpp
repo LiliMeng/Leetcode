78d2c692207a77851099f2b175beaca94995764a
LiliMeng <lilimeng1103@gmail.com>
2015-05-12 11:25:04 -0700

    Update Find_Peak_Element.cpp

--- Find_Peak_Element.cpp
+++ Find_Peak_Element.cpp
@@ -23,7 +23,7 @@ public:
     for(int i=0; i<nums.size();i++)
     {
 
-        if(i>0 and i<nums.size()-1 and nums[i]>nums[i+1] and nums[i]>nums[i-1])
+        if(i>0 and i<nums.size()-1 and nums[i]>nums[i+1] and nums[i]>nums[i-1]) //if imagine You may imagine that num[-1] = num[n] = -∞. i>0 and i<nums.size()-1 could be ignored 
         return i;
         else if(nums[0]>nums[1])
         return 0;
