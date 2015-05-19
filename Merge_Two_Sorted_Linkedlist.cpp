e892d5c29b6e3a38fc7f6d915c064bf1dbe593b2
LiliMeng <lilimeng1103@gmail.com>
2015-05-18 17:59:02 -0700

    Create Merge_Two_Sorted_Linkedlist.cpp

---
+++ Merge_Two_Sorted_Linkedlist.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+
+        ListNode* dummyHead=new ListNode(0);
+        ListNode* p=dummyHead;
+
+        while(l1!=NULL&&l2!=NULL)
+        {
+            if(l1->val<l2->val)
+            {
+                p->next=l1;
+                l1=l1->next;
+            }
+            else
+            {
+                p->next=l2;
+                l2=l2->next;
+            }
+            p=p->next;
+        }
+
+        if(l1!=NULL&&l2==NULL) p->next=l1;
+        if(l1==NULL&&l2!=NULL) p->next=l2;
+
+
+        return dummyHead->next;
+    }
+};
+
+int main()
+{
+
+    ListNode* n1= new ListNode(2);
+    ListNode* n2= new ListNode(4);
+    ListNode* n3= new ListNode(5);
+    ListNode* n4= new ListNode(1);
+    ListNode* n5= new ListNode(3);
+    ListNode* n6= new ListNode(6);
+
+    n1->next=n2;
+    n2->next=n3;
+    n3->next=NULL;
+    n4->next=n5;
+    n5->next=n6;
+    n6->next=NULL;
+
+    Solution s;
+    ListNode* result=s.mergeTwoLists(n1,n4);
+    while(result!=NULL)
+    {
+        cout<<result->val<<endl;
+        result=result->next;
+    }
+
+    return 0;
+}
