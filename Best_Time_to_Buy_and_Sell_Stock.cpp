80c27c1d8c4e03237d974d95c7ae801fd2068bf9
LiliMeng <lilimeng1103@gmail.com>
2015-06-03 12:07:44 -0700

    Update Best_Time_to_Buy_and_Sell_Stock.cpp

--- Best_Time_to_Buy_and_Sell_Stock.cpp
+++ Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -7,26 +7,26 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
 
-        if(prices.size()<=1)
+        if(prices.size()<1)
         {
             return 0;
         }
 
-        int minProfit=prices[0];
+        int minPrice=prices[0];
+        int maxProfit=prices[1]-prices[0];
 
-        int profit = prices[1]-prices[0];
-
-        for(int i=2;i<prices.size();i++)
+        for(int i=1;i<prices.size();i++)
         {
-            minProfit=min(prices[i-1],minProfit);
-            profit=max(profit,prices[i]-minProfit);
+            minPrice=min(prices[i-1],minPrice);
+
+            maxProfit=max(maxProfit,prices[i]-minPrice);
         }
 
-        if(profit<0)
+        if(maxProfit<0)
         {
             return 0;
         }
-        return profit;
+        return maxProfit;
     }
 };
 
@@ -34,7 +34,7 @@ public:
 int main(){
 
     Solution s;
-    vector<int> prices={1,2,3,6};
+    vector<int> prices={6,1,3,2,4,7};
     cout<<s.maxProfit(prices)<<endl;
     return 0;
 }
