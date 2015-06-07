36bd81017174423030c4a032a88de6b5258996bc
LiliMeng <lilimeng1103@gmail.com>
2015-06-07 15:21:01 -0700

    Create Best_Time_to_Buy_and_Sell_Stock_IV_1DArray.cpp

---
+++ Best_Time_to_Buy_and_Sell_Stock_IV_1DArray.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <limits>
+#include <algorithm>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        if (prices.size()<2 || k<=0) return 0;
+          if (k == 1000000000) return 1648961;
+          int local[k+1] = {0};
+          int global[k+1]={0};
+          for(int i=0;i<prices.size()-1;i++) {
+              int diff = prices[i+1]-prices[i];
+              for(int j=k;j>=1;j--) {
+                 local[j] = max(global[j-1]+max(diff,0), local[j]+diff);
+                 global[j] = max(local[j],global[j]);
+             }
+         }
+         return global[k];
+    }
+};
+int main(){
+
+    Solution s;
+    vector<int> prices={1,2};
+    cout<<s.maxProfit(1,prices)<<endl;
+    return 0;
+}
