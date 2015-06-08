ddd1f41282ca6cf301883ac14498229ef53ece3e
LiliMeng <lilimeng1103@gmail.com>
2015-06-08 10:24:59 -0700

    Update Best_Time_to_Buy_and_Sell_Stock_IV_1DArray.cpp

--- Best_Time_to_Buy_and_Sell_Stock_IV_1DArray.cpp
+++ Best_Time_to_Buy_and_Sell_Stock_IV_1DArray.cpp
@@ -9,18 +9,34 @@ using namespace std;
 class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) {
-        if (prices.size()<2 || k<=0) return 0;
-          if (k == 1000000000) return 1648961;
-          int* local = new int[k+1]();
-          int global = new int[k+1]();
-          for(int i=0;i<prices.size()-1;i++) {
-              int diff = prices[i+1]-prices[i];
-              for(int j=k;j>=1;j--) {
-                 local[j] = max(global[j-1]+max(diff,0), local[j]+diff);
-                 global[j] = max(local[j],global[j]);
-             }
-         }
-         return global[k];
+
+       if(prices.size()<2||k<=0) return 0;
+       if(k>prices.size()) return maxProfitLarge(k,prices);
+       int* local=new int[k+1]();
+       int* global=new int[k+1]();
+       for(int i=0;i<prices.size()-1;i++)
+       {
+           int diff=prices[i+1]-prices[i];
+           for(int j=k;j>=1;j--)
+           {
+               local[j]=max(global[j-1]+max(diff,0),local[j]+diff);
+               global[j]=max(local[j],global[j]);
+           }
+       }
+       return global[k];
+    }
+
+    int maxProfitLarge(int k, vector<int>& prices){
+        if(prices.size()<2||k<=0) return 0;
+        int sum=0;
+        for(int i=0;i<prices.size()-1;i++)
+        {
+            if(prices[i+1]-prices[i]>0)
+            {
+                sum=sum+prices[i+1]-prices[i];
+            }
+        }
+        return sum;
     }
 };
 int main(){
