ef04ca827931f376c999cae1f7049d25cf4eba20
LiliMeng <lilimeng1103@gmail.com>
2015-07-16 21:33:35 -0700

    Create  Power_of_Two.cpp

---
+++ Power_of_Two.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        
+          return (n > 0) && (!(n & (n - 1)));
+        
+    }
+};
+
+// If a number n is the power of 2, if written in binary format, the first digit shall be 1, such as (10000,10,1,100, or sth)
+
+//n-1 shall be 11111 or numbers of all 1, For this case n&(n-1) must be 0; !(n&(n-1)) must be 1 
