e4036e89fffdfa267b1546292ba57f7835af946f
LiliMeng <lilimeng1103@gmail.com>
2015-07-25 20:26:52 -0700

    Update Implement_Stack_using_Queues.cpp

--- Implement_Stack_using_Queues.cpp
+++ Implement_Stack_using_Queues.cpp
@@ -2,8 +2,10 @@ class Stack {
 public:
     // Push element x onto stack.
     void push(int x) {
-        
-        while(!q1.empty()){
+        //比如push1,2,3,4到stack s里，s=4,3,2,1->这样pop出来时就是4先pop出来，建一个q1使得q1=1,2,3,4->这样q1的front就是4 跟stack的4一样
+        //先把q1清空，暂时放在q2里，然后再放回q1
+        while(!q1.empty())
+        {
             q2.push(q1.front());
             q1.pop();
         }
@@ -14,23 +16,21 @@ public:
             q2.pop();
         }
     }
- 
+
     // Removes the element on top of the stack.
-    void pop(void) {
+    void pop() {
         q1.pop();
     }
- 
+
     // Get the top element.
-    int top(void) {
+    int top() {
         return q1.front();
     }
- 
+
     // Return whether the stack is empty.
-    bool empty(void) {
+    bool empty() {
         return q1.empty();
     }
-     
-private:
-    queue<int> q1;
-    queue<int> q2;
+    private:
+    queue<int> q1,q2;
 };
