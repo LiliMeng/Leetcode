fa42b0bc0157fda31332824e63c7c98e7f811da9
LiliMeng <lilimeng1103@gmail.com>
2015-07-25 20:32:07 -0700

    Update Implement_Stack_using_Queues.cpp

--- Implement_Stack_using_Queues.cpp
+++ Implement_Stack_using_Queues.cpp
@@ -3,7 +3,8 @@ public:
     // Push element x onto stack.
     void push(int x) {
         //比如push1,2,3,4到stack s里，s=4,3,2,1->这样pop出来时就是4先pop出来，建一个q1使得q1=1,2,3,4->这样q1的front就是4 跟stack的4一样
-        //先把q1清空，暂时放在q2里，然后再放回q1 
+        //先把q1清空，暂时放在q2里，然后再放回q1
+        //外表看起来像1先进入stack的形式4,3,2,1-> 实际上是存的4先进入q1,1,2,3,4->front的q1的形式
         while(!q1.empty())
         {
             q2.push(q1.front());
