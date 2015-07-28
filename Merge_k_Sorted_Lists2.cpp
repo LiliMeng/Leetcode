96f9f9bce89188b4482460bbe12fd8ddc520e44c
LiliMeng <lilimeng1103@gmail.com>
2015-07-28 10:34:15 -0700

    Update Merge_k_Sorted_Lists2.cpp

--- Merge_k_Sorted_Lists2.cpp
+++ Merge_k_Sorted_Lists2.cpp
@@ -1,25 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
-    struct compNode {
-        bool operator()(ListNode *p, ListNode *q) const {
-            return p->val>q->val;
-        }  
+    struct compare{
+        bool operator()(ListNode* node1, ListNode* node2) const {
+            return node1->val>node2->val;
+        }
     };
-
-    ListNode *mergeKLists(vector<ListNode *> &lists) {
-        priority_queue<ListNode*, vector<ListNode*>, compNode> pq;
-        ListNode *dummy = new ListNode(0), *tail = dummy;
+    
+    ListNode* mergeKLists(vector<ListNode* > &lists){
+        priority_queue<ListNode*, vector<ListNode*>, compare> pq;
+        ListNode* dummyHead=new ListNode(0);
+        ListNode* p=dummyHead;
         
-        for(int i=0; i<lists.size(); i++) 
-            if(lists[i]) pq.push(lists[i]);
-            
-        while(!pq.empty()) {
-            tail->next = pq.top();
-            tail = tail->next;
-            pq.pop();
-            if(tail->next) pq.push(tail->next);
+        for(int i=0; i<lists.size(); i++)
+        {
+            if(lists[i]!=NULL)
+            {
+                pq.push(lists[i]);
+            }
         }
         
-        return dummy->next;
+        while(!pq.empty())
+        {
+            p->next=pq.top();
+            p=p->next;
+            pq.pop();
+            if(p->next!=NULL)
+            {
+                pq.push(p->next);
+            }
+        }
+        
+        return dummyHead->next;
     }
+    
 };
