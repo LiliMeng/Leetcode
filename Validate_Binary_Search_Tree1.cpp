5aa1f816b7b82f93746ef241f45bcc8e5e627980
LiliMeng <lilimeng1103@gmail.com>
2015-07-27 19:55:00 -0700

    Update Validate_Binary_Search_Tree1.cpp

--- Validate_Binary_Search_Tree1.cpp
+++ Validate_Binary_Search_Tree1.cpp
@@ -27,3 +27,27 @@ private:
         return helper(root->left, minV, root->val)&&helper(root->right, root->val, maxV);
     }
 };
+
+或者
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        
+        return helper(root, LONG_MIN, LONG_MAX);
+    }
+    
+private:
+    bool helper(TreeNode* root, long minV, long maxV)
+    {
+        if(root==NULL) return true;
+        
+        if(root->val>minV&&root->val<maxV)
+        {
+            return helper(root->left, minV, root->val)&&helper(root->right, root->val, maxV);
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
