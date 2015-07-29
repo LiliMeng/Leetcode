248ec63496170cd7819600a38ecec655b1c5f8e1
LiliMeng <lilimeng1103@gmail.com>
2015-07-29 15:40:29 -0700

    Update Number_of_1_Bits.cpp

--- Number_of_1_Bits.cpp
+++ Number_of_1_Bits.cpp
@@ -12,3 +12,7 @@ public:
     return count;  
     }
 };
+
+// if n!=0, count++, and then n=(n-1)&n; if n is the power of 2, there is definitely one 1 digit. 
+
+// if n is the power of 2, (n-1)&n=0, n=0, there is only one 1 digit; if n is not power of 2, and n!=0, (n-1)&n will be 1, and loop till n is the power of two, in that situation, there is only one 1 digit left
