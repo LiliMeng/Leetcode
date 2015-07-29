db9255acc58621e676ec98e8746f895ad5a4cebc
LiliMeng <lilimeng1103@gmail.com>
2015-07-29 15:41:30 -0700

    Update Number_of_1_Bits.cpp

--- Number_of_1_Bits.cpp
+++ Number_of_1_Bits.cpp
@@ -15,4 +15,4 @@ public:
 
 // if n!=0, count++, and then n=(n-1)&n; if n is the power of 2, there is definitely one 1 digit. 
 
-// if n is the power of 2, (n-1)&n=0, n=0, there is only one 1 digit; if n is not power of 2, and n!=0, (n-1)&n will be 1, and loop till n is the power of two, in that situation, there is only one 1 digit left
+// if n is the power of 2, (n-1)&n=0, n=0, there is only one 1 digit; if n is not power of 2, and n!=0, (n-1)&n will be 1, and loop till n is the power of two(循环到最高位), in that situation, there is only one 1 digit left
