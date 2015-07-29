39de08c133030f9599ad6366c619b08df6dacacf
LiliMeng <lilimeng1103@gmail.com>
2015-07-29 11:37:24 -0700

    Update Reverse_Linked_List.cpp

--- Reverse_Linked_List.cpp
+++ Reverse_Linked_List.cpp
@@ -9,17 +9,25 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
+       
+        ListNode* prev=NULL;
+        ListNode* current=head;
         
-         ListNode* prev = NULL;
-         while (head != NULL) {
-            ListNode* temp = head->next;
-            head->next = prev;
-            prev = head;
-            head = temp;
-         }
+        while(current!=NULL)
+        {
+            ListNode* temp=current->next; 
+            current->next=prev;  //把每次current指针指到的都放在prev的前面
+            prev=current;  //prev是当前的current  把current放在prev之前 然后再更新prev成为old current
+            current=temp;  //current指针在不断前进一直走到链表最后
+        }
         return prev;
-        
     }
 };
+
+NULL 1, 2, 3,4 第一次 prev=NULL, current=1 
+1,NULL,2,3,4, 第二次 prev=1, current=2 
+2,1，NULL,3,4; prev=2, current=3
+3,2,1,NULL,4   prev=3, current=4
+4,3,2,1,NULL   prev=4 current=NULL 结束循环 不再进行下一次
 
 //http://articles.leetcode.com/2010/04/reversing-linked-list-iteratively-and.html
