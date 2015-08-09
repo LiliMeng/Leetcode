f25ccb25d6e44152b0c958581e6781896b76ae09
LiliMeng <lilimeng1103@gmail.com>
2015-08-09 11:20:42 -0700

    Update Add_Binary.cpp

--- Add_Binary.cpp
+++ Add_Binary.cpp
@@ -5,15 +5,15 @@ public:
         int la = a.size() - 1;
         int lb = b.size() - 1;
         string result = la>=lb?a:b;
-        int carryon = 0;
+        int carrier = 0;
         for (int i = result.size() - 1; i >= 0; i--){
-            int c = carryon;
+            int c = carrier;
             if (la >= 0) c =c+ a[la--] - '0';
             if (lb >= 0) c =c+ b[lb--] - '0';
             result[i] = c%2  + '0';
-            carryon = c/2;
+            carrier = c/2;
         }
-        if (carryon==1)
+        if (carrier==1)
             result="1"+result;
         return result;
     }
