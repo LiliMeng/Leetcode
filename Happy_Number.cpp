b9008babf164a55ea113117b2064941e26f5f4f4
LiliMeng <lilimeng1103@gmail.com>
2015-08-10 10:53:19 -0700

    Update Happy_Number.cpp

--- Happy_Number.cpp
+++ Happy_Number.cpp
@@ -3,16 +3,28 @@ public:
     bool isHappy(int n) {
         
         unordered_map<int, int> m;
-        while (n != 1) {
+        
+        while (n != 1)
+        {
             int sum = 0;
-            while (n > 0) {
+            while (n > 0) 
+            {
                 sum += ((n % 10) * (n % 10));
                 n /= 10;
             }
+            
             n = sum;
-            if (m.find(n) != m.end()) break;
-            else m[n] = 1;
+            
+            if (m.find(n) != m.end()) 
+            {
+                break;
+            }
+            else 
+            {
+                m[n] = 1;
+            }
         }
+        
         return n == 1;
         
     }
