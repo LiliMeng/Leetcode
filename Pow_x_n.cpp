3ca13862c066a58d44cb437fd5af0fb518b842a8
LiliMeng <lilimeng1103@gmail.com>
2015-08-10 12:21:49 -0700

    Update Pow_x_n.cpp

--- Pow_x_n.cpp
+++ Pow_x_n.cpp
@@ -1,15 +1,17 @@
 class Solution {
+//分两种情况,一种n>0, 一种n<0的情况
+class Solution {
 public:
     double myPow(double x, int n) {
         
         if(n==0) return 1;
         if(n==1) return x;
         
-        double tmp=pow(x, abs(n/2));
+        double tmp=pow(x, abs(n/2)); //为啥要abs(n/2)呢?
         
         if(n>0)
         {
-            if(n&1)
+            if(n&1)    
             {
                 return tmp*tmp*x;
             }
