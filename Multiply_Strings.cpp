285ac7f4fa252dc9cb4e68e5ba94f299e37764e1
LiliMeng <lilimeng1103@gmail.com>
2015-08-09 22:17:13 -0700

    Update Multiply_Strings.cpp

--- Multiply_Strings.cpp
+++ Multiply_Strings.cpp
@@ -2,26 +2,32 @@ class Solution {
 public:
     string multiply(string num1, string num2) {
         
-       if(num1.empty() || num2.empty())
-            return "";
-        int s1 = num1.size();
-        int s2 = num2.size();
-        string ret(s1+s2, '0');
-        int i, j;
-        for(i=s1-1; i>=0; --i){
-            int carry = 0;
-            for(j=s2-1; j>=0; --j){
-                int tmp = (ret[i+j+1]-'0')+(num1[i]-'0')*(num2[j]-'0')+carry;
-                ret[i+j+1] = '0'+tmp%10;
-                carry = tmp/10;
+        string res(num1.size()+num2.size(), '0');
+        
+        for(int i=num1.size()-1, d=res.size()-1; i>=0; i--, d--)
+        {
+            int carry=0, k=d;
+            
+            for(int j=num2.length()-1; j>=0; j--, k--)
+            {
+                int a=num1[i]-'0';
+                int b=num2[j]-'0';
+                a=a*b+carry+(res[k]-'0');
+                carry=a/10;
+                res[k]=a%10 + '0';
             }
-            ret[i]+=carry;
+            
+            while(carry!=0)
+            {
+                int sum=carry+(res[k]-'0');
+                carry=sum/10;
+                res[k--]=sum%10+'0';
+            }
         }
-        //skip the leading 0's
-        for(i=0; i<s1+s2; ++i){
-            if(ret[i]!='0')
-                return ret.substr(i, s1+s2-i);
-        }
-        return "0";
+        
+        while(res.size()>1&&res[0]=='0') res.erase(res.begin());
+        
+        return res;
+        
     }
 };
