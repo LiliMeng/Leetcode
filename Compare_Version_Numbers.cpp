20786e42cd3cc9bd888ab8e1921d1023b6f660fd
LiliMeng <lilimeng1103@gmail.com>
2015-08-11 15:48:02 -0700

    Update Compare_Version_Numbers.cpp

--- Compare_Version_Numbers.cpp
+++ Compare_Version_Numbers.cpp
@@ -6,31 +6,36 @@ using namespace std;
 class Solution {
 public:
     int compareVersion(string version1, string version2) {
-
-        int index1 = 0;
-        int index2 = 0;
-
-        while(index1 < version1.size() || index2 < version2.size())
+        
+        int i=0;
+        int j=0;
+        
+        while(i<version1.size()||j<version2.size())
         {
-            int curSum1 = 0;
-            int curSum2 = 0;
-            while(index1 < version1.size() && version1[index1] != '.')
+            int sum1 = 0;
+            int sum2 = 0;
+            
+            while(i<version1.size()&&version1[i]!='.')
             {
-                curSum1 = curSum1*10 + version1[index1++]-'0';
-                index1++;
+                sum1=sum1*10+version1[i++]-'0';
             }
-         while(index2 < version2.size() && version2[index2] != '.')
-         {
-             curSum2 = curSum2*10 + version2[index2++]-'0';
-             index2++;
-         }
-        if(curSum1 > curSum2) return 1;
-        if(curSum1 < curSum2) return -1;
-    }
+            
+            while(j<version2.size()&&version2[j]!='.')
+            {
+                sum2=sum2*10+version2[j++]-'0';
+            }
+            
+            i++;
+            j++;
+        
+        
+        if(sum1 > sum2) return 1;
+        if(sum1 < sum2) return -1;
+        
+        }
     return 0;
     }
 };
-
 
 int main()
 {
