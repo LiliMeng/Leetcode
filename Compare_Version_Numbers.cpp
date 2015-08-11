6a19760eb6a1df9b4280a6726591810670eb4ec3
LiliMeng <lilimeng1103@gmail.com>
2015-08-11 15:53:23 -0700

    Update Compare_Version_Numbers.cpp

--- Compare_Version_Numbers.cpp
+++ Compare_Version_Numbers.cpp
@@ -31,7 +31,7 @@ public:
         
         if(sum1 > sum2) return 1;
         if(sum1 < sum2) return -1;
-        
+        //这时如果sum1!=sum2就重新开始进行下一个循环 比较下一步 比如 1.123, 1.234. 最初1都相同, 进行下一轮比较123, 234
         }
     return 0;
     }
