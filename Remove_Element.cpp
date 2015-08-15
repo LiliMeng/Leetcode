62dd383858b0f6ebfe57d171b8433df2cc77cf43
LiliMeng <lilimeng1103@gmail.com>
2015-08-14 20:08:12 -0700

    Update Remove_Element.cpp

--- Remove_Element.cpp
+++ Remove_Element.cpp
@@ -1,3 +1,6 @@
+//这里不能用erase() 因为是in place
+//The order of elements can be changed. It doesn't matter what you leave beyond the new length
+//把val的值移动到最后面 比如{1,2,3,4,3,5,3,6} 移动成{1,2,4,5,6,3,3,3}所以后面怎么样没关系的 数组最重要的是下标
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
