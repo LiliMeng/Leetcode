3096d07603d40be1ba0c5e515723d2d4a659c17a
LiliMeng <lilimeng1103@gmail.com>
2015-08-14 20:11:16 -0700

    Update Remove_Element.cpp

--- Remove_Element.cpp
+++ Remove_Element.cpp
@@ -1,21 +1,22 @@
 //这里不能用erase() 因为是in place
 //The order of elements can be changed. It doesn't matter what you leave beyond the new length
 //把val的值移动到最后面 比如{1,2,3,4,3,5,3,6} 移动成{1,2,4,5,6,3,3,3}所以后面怎么样没关系的 数组最重要的是下标
+//这里用的vector<int>& 是reference 处理完这个函数后nums也变了. 如果要nums不变的话 需要用const vector<int>&
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
         
-        int cur=0;
+        int j=0;
         for(int i=0; i<nums.size(); i++)
         {
             if(nums[i]==val)
             {
                 continue;
             }
-            nums[cur]=nums[i];
-            cur++;
+            nums[j]=nums[i];
+            j++;
         }
         
-        return cur;
+        return j;
     }
 };
