4e0b85dfb66ebfb60bc274aa2152b2a4b270058f
LiliMeng <lilimeng1103@gmail.com>
2015-08-16 11:43:44 -0700

    Update Unique_Binary_Search_Trees.cpp

--- Unique_Binary_Search_Trees.cpp
+++ Unique_Binary_Search_Trees.cpp
@@ -1,63 +1,35 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-
-using namespace std;
-
-struct TreeNode{
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x): val(x),left(NULL),right(NULL) {}
-};
-
-class Solution{
+class Solution {
 public:
-    vector<TreeNode*> generateTrees(int n){
-
+    vector<TreeNode*> generateTrees(int n) {
+        
         return generate(1,n);
     }
-
-    vector<TreeNode*> generate(int start, int stop)
+    
+    vector<TreeNode*> generate(int start, int end)
     {
         vector<TreeNode*> vs;
-        if(start > stop){
+        if(start>end)
+        {
             vs.push_back(NULL);
             return vs;
         }
-
-        for(int i=start; i<=stop; i++)
+        
+        for(int i=start; i<=end; i++)
         {
-            auto l=generate(start,i-1);
-            auto r=generate(i+1,stop);
-
-            for(int j=0;j<l.size();j++){
-                for(int k=0;k<r.size();k++){
-                TreeNode* n=new TreeNode(i);
-                n->left=l[j];
-                n->right=r[k];
-                vs.push_back(n);
+            auto l=generate(start, i-1);
+            auto r=generate(i+1, end);
+            
+            for(int j=0; j<l.size(); j++)
+            {
+                for(int k=0; k<r.size(); k++)
+                {
+                    TreeNode* n=new TreeNode(i);
+                    n->left=l[j];
+                    n->right=r[k];
+                    vs.push_back(n);
                 }
             }
         }
-
         return vs;
     }
-
 };
-
-int main(){
-
-    Solution s;
-
-    vector<TreeNode*> gT;
-    gT=s.generateTrees(3);
-
-    for(auto ite=gT.begin();ite!=gT.end();ite++)
-    {
-        cout<<*ite<<endl;
-    }
-
-    return 0;
-
-}
