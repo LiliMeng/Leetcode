31d4f1015c927afcb89daec800f3811feb01c5e1
LiliMeng <lilimeng1103@gmail.com>
2015-08-15 17:36:31 -0700

    Create Integer_to_RomanI.cpp

---
+++ Integer_to_RomanI.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        
+        string str;    
+        string symbol[13]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};    
+        int value[13]=    {1000,900,500,400, 100, 90,  50, 40,  10, 9,   5,  4,   1};   
+        for(int i=0;num!=0;++i)  
+        {  
+            while(num>=value[i])  
+            {  
+                num-=value[i];  
+                str+=symbol[i];  
+            }  
+        }  
+        return str;  
+    }
+};
