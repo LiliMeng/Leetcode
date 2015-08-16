cbcb64e1cdb0ffb91e10c2dd1a3e05497f88abf9
LiliMeng <lilimeng1103@gmail.com>
2015-08-15 17:03:00 -0700

    Update Roman_to_Integer.cpp

--- Roman_to_Integer.cpp
+++ Roman_to_Integer.cpp
@@ -1,28 +1,29 @@
+//IV代表4 (V-I=5-1=4)   VI代表6(V+I=5+1=6)
 class Solution {
 public:
     int romanToInt(string s) {
         
-        int map[26];
-        map['I'-'A'] = 1;    //IV代表4 (V-I=5-1=4)   VI代表6(V+I=5+1=6)
-        map['V'-'A'] =5;
-        map['X'-'A']=10;
-        map['L'-'A']=50;
-        map['C'-'A']=100;
-        map['D'-'A']=500;
-        map['M'-'A']=1000;
+        unordered_map<int, int> htable;
+        htable.insert({'I'-'A',1});
+        htable.insert({'V'-'A',5});
+        htable.insert({'X'-'A',10});
+        htable.insert({'L'-'A',50});
+        htable.insert({'C'-'A',100});
+        htable.insert({'D'-'A',500});
+        htable.insert({'M'-'A',1000});
         int res=0;
         int n=s.size();
         s.push_back(s[n-1]);
         
         for(int i=0; i<n; i++)
         {
-            if(map[s[i]-'A']>=map[s[i+1]-'A'])
+            if(htable[s[i]-'A']>=htable[s[i+1]-'A'])
             {
-                res=res+map[s[i]-'A'];
+                res=res+htable[s[i]-'A'];
             }
             else
             {
-                res=res-map[s[i]-'A'];
+                res-=htable[s[i]-'A'];
             }
         }
        
