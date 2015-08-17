a915c2fd80da67ff4c43b202ffa98ca55415d41b
LiliMeng <lilimeng1103@gmail.com>
2015-08-16 17:36:51 -0700

    Update Search_a_2D_Matrix.cpp

--- Search_a_2D_Matrix.cpp
+++ Search_a_2D_Matrix.cpp
@@ -19,7 +19,7 @@ public:
         while(start<=end)
         {
             int middle=(start+end)/2;
-            int x= middle/n;
+            int x= middle/n;   //这里的关键在于middle/n middle是一群数的中点, 每行是n个  middle/n是中间值所处的行数, 每行放5个, middle/5 就是已经分配了几行了 因为这里是从0行开始,所以middle/5是middle所在的行数, 给了几行后, middle%5就是所在的列数
             int y= middle%n;
             if(matrix[x][y]==target)
             {
