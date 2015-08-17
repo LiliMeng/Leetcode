6781d8482a86859aa93eef9d4c769447415149d3
LiliMeng <lilimeng1103@gmail.com>
2015-08-16 21:23:03 -0700

    Update Product_of_Array_Except_Self.cpp

--- Product_of_Array_Except_Self.cpp
+++ Product_of_Array_Except_Self.cpp
@@ -4,7 +4,33 @@
 //[1, a1, a1*a2, a1*a2*a3]
 //[a2*a3*a4, a3*a4, a4, 1]
 
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+     
+        int n=nums.size();
+        
+        vector<int> res(n,1);
+        vector<int> r(n,1);
+        for(int i=1; i<n; i++)
+        {
+            res[i]=res[i-1]*nums[i-1];
+        }
+        
+        for(int i=n-2; i>=0; i--)
+        {
+            r[i]=r[i+1]*nums[i+1];
+        }
+        
+        for(int i=0; i<n; i++)
+        {
+            res[i]=res[i]*r[i];
+        }
+        return res;
+    }
+};
 
+//算法2
 
 class Solution {
 public:
