2cbb80ccce93a2113e754d08f3ca10fb90cf0d08
LiliMeng <lilimeng1103@gmail.com>
2015-08-17 17:44:13 -0700

    Update Lowest_Common_of_a_Binary_Tree.cpp

--- Lowest_Common_of_a_Binary_Tree.cpp
+++ Lowest_Common_of_a_Binary_Tree.cpp
@@ -1,26 +1,26 @@
+//这个假设如果在一个树上只能找到p, 找不到q时 就返回p, 而不是返回null, 只有在一棵树上既找不到p 又找不到q时 才返回null
+
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         
         if(root==NULL||p==root||q==root) return root;
+        
         TreeNode* left=lowestCommonAncestor(root->left, p, q);
         TreeNode* right=lowestCommonAncestor(root->right, p, q);
         
-        if(left!=NULL&&right!=NULL)
+        if(left==NULL&&right!=NULL)
+        {
+            return right;
+        }
+        else if(left!=NULL&&right==NULL)
+        {
+            return left;
+        }
+        else if(left!=NULL&&right!=NULL)
         {
             return root;
         }
-        else
-        {
-            if(left!=NULL)
-            {
-                return left;
-            }
-            else
-            {
-                return right;
-            }
-            
-        }
+        
     }
 };
