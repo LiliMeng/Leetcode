6f611b29ab6d5f32e4013aaf7d4e383992d3edc6
LiliMeng <lilimeng1103@gmail.com>
2015-08-18 12:55:43 -0700

    Update Basic_Calculator.cpp

--- Basic_Calculator.cpp
+++ Basic_Calculator.cpp
@@ -1,42 +1,51 @@
+class Solution {
 public:
     int calculate(string s) {
+        
         stack<char> signs;
         signs.push(1);
         int sign=1;
-        int ans=0;
+        int res=0;
         int num=0;
-        for(char c : s)
+        
+        for(int i=0; i<s.size(); i++)
         {
-            if(c>='0' && c<='9')
+            if(s[i]>='0' && s[i]<='9')
             {
-                num=num*10+c-'0';
+                num=num*10+s[i]-'0';
             }
-            else if(c=='+' || c=='-')
+            else if(s[i]=='+'||s[i]=='-')
             {
-                ans=ans+signs.top()*sign*num;
+                res=res+signs.top()*sign*num;
                 num=0;
-                sign=c=='+'?1:-1;
+                if(s[i]=='+')
+                {
+                    sign=1;
+                }
+                else
+                {
+                    sign=-1;
+                }
             }
-            else if(c=='(')
+            else if(s[i]=='(')
             {
                 signs.push(signs.top()*sign);
                 sign=1;
             }
-            else if(c==')')
+            else if(s[i]==')')
             {
-                ans=ans+signs.top()*sign*num;
+                res=res+signs.top()*sign*num;
                 signs.pop();
                 sign=1;
                 num=0;
             }
-            
         }
         
-        if(num)
+        if(num!=0)
         {
-            ans=ans+signs.top()*sign*num;
+            res=res+signs.top()*sign*num;
         }
         
-        return ans;
+        return res;
     }
 };
