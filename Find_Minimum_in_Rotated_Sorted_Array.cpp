51f345fbec33757de120893aedbf4f228efb0a05
LiliMeng <lilimeng1103@gmail.com>
2015-08-20 10:30:29 -0700

    Update Find_Minimum_in_Rotated_Sorted_Array.cpp

--- Find_Minimum_in_Rotated_Sorted_Array.cpp
+++ Find_Minimum_in_Rotated_Sorted_Array.cpp
@@ -7,13 +7,13 @@ public:
         while(start<end)
         {
             int mid=start+(end-start)/2;
-            if(nums[mid]<nums[end])
+            if(nums[mid]<nums[end])   //if nums[mid]<nums[end], [mid, end] has already been sorted, so the minimum will be in [0, mid], that is to say end=mid.
             {
                 end = mid;
             }
             else
             {
-                start = mid+1;
+                start = mid+1; //if nums[mid]>=nums[end], [mid, end] is not sorted, so the minimum will be in [mid+1, end], start=mid+1 
             }
         }
         
