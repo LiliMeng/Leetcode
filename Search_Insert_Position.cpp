31b79fcd919340ab5c7d0a6394706ac658c80b5e
LiliMeng <lilimeng1103@gmail.com>
2015-08-21 11:45:55 -0700

    Update Search_Insert_Position.cpp

--- Search_Insert_Position.cpp
+++ Search_Insert_Position.cpp
@@ -25,3 +25,45 @@ public:
         }
     }
 };
+
+//Binary Search Complexity log(n)
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+         int start=0;
+        int end=nums.size()-1;
+
+
+        if(target<nums[0])
+        {
+            return 0;
+        }
+        if(target>nums[nums.size()-1])
+        {
+            return nums.size();
+        }
+
+
+        while(start<end)
+        {
+            int mid=(start+end)/2;
+
+            if(mid>start&&nums[mid]>target&&nums[mid-1]<target) return mid;
+
+            if(nums[mid]<target)
+            {
+                start=mid+1;
+            }
+            else if(nums[mid]>target)
+            {
+                end=mid-1;
+            }
+            else
+            {
+               return mid;
+            }
+
+        }
+    }
+};
