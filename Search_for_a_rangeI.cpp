7506645c68e94926963ee23c6a6f08c85e6a4ef0
LiliMeng <lilimeng1103@gmail.com>
2015-08-21 10:09:00 -0700

    Create Search_for_a_rangeI.cpp

---
+++ Search_for_a_rangeI.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        
+       vector<int> res;
+       res.push_back(findleft(nums,target));
+       res.push_back(findright(nums,target));
+       return res;
+    } 
+    
+    int findleft(vector<int>& nums, int target)
+    {
+        int start=0;
+        int end=nums.size()-1;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(nums[mid]<target)
+            {
+                start=mid+1;
+            }
+            else if(nums[mid]>target)
+            {
+                end=mid-1;
+            }
+            else
+            {
+                end=mid-1;
+            }
+        }
+        
+        if(start>=0&&start<nums.size()&&nums[start]==target)
+        {
+            return start;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+    
+    int findright(vector<int>& nums, int target)
+    {
+        int start =0;
+        int end=nums.size()-1;
+        
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(nums[mid]<target)
+            {
+                start=mid+1;
+            }
+            else if(nums[mid]>target)
+            {
+                end=mid-1;
+            }
+            else
+            {
+                start=mid+1;
+            }
+        }
+        
+        if(end>=0&&end<nums.size()&&nums[end]==target)
+        {
+            return end;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+};
