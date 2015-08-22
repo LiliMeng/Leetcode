c46e2e06a1968962e2609bb16bae2d4bc13087aa
LiliMeng <lilimeng1103@gmail.com>
2015-08-22 13:56:56 -0700

    Create Search_in_Rotated_Sorted_ArrayII.cpp

---
+++ Search_in_Rotated_Sorted_ArrayII.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        
+        int start =0;
+        int end=nums.size()-1;
+        
+        while(start<=end)
+        {
+            int mid=(start+end)/2;
+            
+            if(nums[mid]==target) return true;
+            
+            if(nums[start]<nums[mid])    //left half sorted
+            {
+                
+                if(target>=nums[start]&&target<nums[mid])
+                {
+                    end=mid;
+                }
+                else
+                {
+                    start=mid+1;
+                }
+            }
+            
+            else if(nums[start]>nums[mid])  //right half sorted
+            {
+                if(target>nums[mid]&&target<=nums[end])
+                {
+                    start=mid+1;
+                }
+                else
+                {
+                    end=mid-1;
+                }
+             }
+             else
+             {
+                 start++;
+             }
+        }
+        
+        return false;
+    }
+};
