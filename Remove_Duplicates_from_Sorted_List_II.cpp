4b451de9b63f07d4fe4394b046fd1e1379328c7b
LiliMeng <lilimeng1103@gmail.com>
2015-08-23 11:31:28 -0700

    Update Remove_Duplicates_from_Sorted_List_II.cpp

--- Remove_Duplicates_from_Sorted_List_II.cpp
+++ Remove_Duplicates_from_Sorted_List_II.cpp
@@ -28,8 +28,10 @@ public:
     }
 };
 
-//搞两个指针，start和end end是最后一个不一样的值 比如 1->1->1->  最初start指向第一个1之前的prev, end指向第一个1 start->next->val==end->next->val这样end
-//就不断往后移动,直到end是第四个1(最后一个一样的) 
+//搞两个指针，start和end end是最后一个不一样的值 比如 1->1->1->1  最初start指向第一个1之前的prev, end指向第一个1 
+//start->next->val==end->next->val这样end就不断往后移动,直到end是第四个1(最后一个一样的)  如果start->next==end 这样就是链表
+//1->2->3 没有执行内while循环这种, start和end指针各向后移动一个  如果是 1->1->1->1->2这种情况 start->next=end->next跳过这四个1
+
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
