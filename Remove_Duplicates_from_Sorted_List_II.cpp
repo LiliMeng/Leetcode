2413cbe141eff849372ec6c908b26997ba1cd138
LiliMeng <lilimeng1103@gmail.com>
2015-08-23 11:27:43 -0700

    Update Remove_Duplicates_from_Sorted_List_II.cpp

--- Remove_Duplicates_from_Sorted_List_II.cpp
+++ Remove_Duplicates_from_Sorted_List_II.cpp
@@ -27,3 +27,39 @@ public:
     
     }
 };
+
+//搞两个指针，start和end end是最后一个不一样的值 比如 1->1->1->  最初start指向第一个1之前的prev, end指向第一个1 start->next->val==end->next->val这样end
+//就不断往后移动,直到end是第四个1(最后一个一样的) 
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        
+        ListNode* dummyHead = new ListNode(INT_MIN);
+        dummyHead->next = head;
+
+        ListNode* start=dummyHead;
+        ListNode* end=head;
+
+
+        while(end!=NULL)
+        {
+            while(end->next!=NULL&&start->next->val==end->next->val)
+            {
+                end=end->next;
+            }
+
+            if(start->next==end)
+            {
+                start=start->next;
+            }
+            else
+            {
+                start->next=end->next;
+            }
+
+            end=end->next;
+        }
+
+        return dummyHead->next;
+    }
+};
