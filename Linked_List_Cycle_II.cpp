b2869c29bac16d366f350a75c4663cd71dd4a94b
LiliMeng <lilimeng1103@gmail.com>
2015-08-23 13:51:44 -0700

    Update Linked_List_Cycle_II.cpp

--- Linked_List_Cycle_II.cpp
+++ Linked_List_Cycle_II.cpp
@@ -2,49 +2,38 @@ class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
         
-       ListNode* slow1=head;
-       ListNode* slow2=hasCycle(head);
-
-        if(hasCycle(head)==NULL)
+        if(head==NULL||head->next==NULL)
         {
             return NULL;
         }
-        else
-        {
-            while(slow1->next!=NULL&&slow2->next!=NULL)
-           {
-                if(slow1==slow2)
-                {
-                    return slow1;
-                }
-                
-                slow1=slow1->next;
-                slow2=slow2->next;
-            }
-        }
-
-
-    }
-
-    ListNode* hasCycle(ListNode* head)
-    {
-        if(head==NULL||head->next==NULL) return false;
-
+        
         ListNode* slow=head;
         ListNode* fast=head;
-
+        ListNode* tmp=head;
+        
         while(fast->next!=NULL&&fast->next->next!=NULL)
         {
             slow=slow->next;
             fast=fast->next->next;
-
+            
             if(slow==fast)
             {
-                return slow;
+                while(slow->next!=NULL&&tmp->next!=NULL)
+                {
+                    if(slow==tmp)
+                    {
+                        return slow;
+                    }
+                    else
+                    {
+                        slow=slow->next;
+                        tmp=tmp->next;
+                    }
+                }
             }
         }
-
+        
         return NULL;
     }
-
+    
 };
