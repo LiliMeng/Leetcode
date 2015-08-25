6fb7a1d64e4cb17b8e41e4496dfd988d63fd2993
LiliMeng <lilimeng1103@gmail.com>
2015-08-25 16:39:28 -0700

    Update Binary_Tree_Level_Order_Traversal_II_latestI.cpp

--- Binary_Tree_Level_Order_Traversal_II_latestI.cpp
+++ Binary_Tree_Level_Order_Traversal_II_latestI.cpp
@@ -14,9 +14,11 @@ public:
         {
             return;
         }
-        res[level].push_back(root->val);
-        DFS(res,root->left, level-1);
-        DFS(res, root->right, level-1);
+        res[level].push_back(root->val);  //第一次res[2].push_back(3); 
+        DFS(res,root->left, level-1);     //root->left为9， 然后 res[1].push_back(9); 里面又有DFS(res, root->left->left, level-2), DFS(RES, root->left->right, level-2)均为NULL 因为9的左树右树均为NULL返回
+        DFS(res, root->right, level-1);   //root->right为20， res[1].push_back(20); 这个函数里面又有
+        //DFS(res, root->right->left, level-2) res[0]={15}, DFS(res, root->right->right, level-2) res[0]={15,7}再往下一层都是NULL 不再进行了
+        
     }
     
     
