6a256610733466d0e6049795e4b6adbc46f8f64f
LiliMeng <lilimeng1103@gmail.com>
2015-08-26 17:43:59 -0700

    Update Number_of_Islands.cpp

--- Number_of_Islands.cpp
+++ Number_of_Islands.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
+        
         int m = grid.size();
-        if (m == 0) return 0;
         int n = grid[0].size();
-        if (n == 0) return 0;
+        if (m==0||n == 0) return 0;
 
-        int ans = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        int res = 0;
+        for (int i = 0; i < m; i++) 
+        {
+            for (int j = 0; j < n; j++) 
+            {
                 if (grid[i][j] != '1') continue;
 
-                ans++;
+                res++;
                 dfs(grid, i, j);
             }
         }
-        return ans;
+        return res;
     }
-
 
     void dfs(vector<vector<char>> &grid, int i, int j) {
 
@@ -27,7 +28,8 @@ public:
 
         if (i < 0 || i >= m || j < 0 || j >= n) return;
 
-        if (grid[i][j] == '1') {
+        if (grid[i][j] == '1') 
+        {
             grid[i][j] = '2';
             dfs(grid, i - 1, j);
             dfs(grid, i + 1, j);
