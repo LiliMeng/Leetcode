0299051941f1ed596ddae32874e2f15398df08d6
LiliMeng <lilimeng1103@gmail.com>
2015-08-26 18:00:36 -0700

    Update Number_of_Islands.cpp

--- Number_of_Islands.cpp
+++ Number_of_Islands.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        
         int m = grid.size();
+        if(m==0) return 0;
         int n = grid[0].size();
-        if (m==0||n == 0) return 0;
+        if (n == 0) return 0;
 
-        int res = 0;
-        for (int i = 0; i < m; i++) 
-        {
-            for (int j = 0; j < n; j++) 
-            {
-                if (grid[i][j] != '1') continue;
-
-                res++;
+        int ans = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '0'||grid[i][j]=='2') 
+                {
+                    continue;
+                }
+                ans++;
                 dfs(grid, i, j);
             }
         }
-        return res;
+        return ans;
     }
+
 
     void dfs(vector<vector<char>> &grid, int i, int j) {
 
@@ -28,8 +29,7 @@ public:
 
         if (i < 0 || i >= m || j < 0 || j >= n) return;
 
-        if (grid[i][j] == '1') 
-        {
+        if (grid[i][j] == '1') {
             grid[i][j] = '2';
             dfs(grid, i - 1, j);
             dfs(grid, i + 1, j);
@@ -38,3 +38,19 @@ public:
         }
     }
 };
+
+/*
+所有的横竖连成一片的算一个岛，Example1的一片１全都连在一起了算是一个岛
+
+11110
+11010
+11000
+00000
+
+Example2的前两行的４个１连成一个岛，第三行的一个１算一个岛，　最后一行的两个１算一个岛
+11000
+11000
+00100
+00011
+
+*/
