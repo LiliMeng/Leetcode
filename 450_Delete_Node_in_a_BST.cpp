4fb1c5305acc8c6cac01c3219b04e8211456738a
LiliMeng <lilimeng1103@gmail.com>
2016-11-05 16:36:15 -0700

    Create 450_Delete_Node_in_a_BST.cpp

---
+++ 450_Delete_Node_in_a_BST.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *      int val;
+ *      TreeNode *left
+ *      TreeNode *right;
+ *      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *  };
+ **/
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key)
+    {
+        TreeNode* tmp=root;
+        if(root==NULL)
+        {
+            return NULL;
+        }
+        else
+        {
+            if(key==tmp->val)
+            {    
+                delete tmp;
+                return tmp;
+            }
+            else if(key<tmp->val)
+            {
+                tmp=tmp->left;
+                return deleteNode(tmp->left, key);
+            }
+            else 
+            {
+                tmp=tmp->right;
+                return deleteNode(tmp->right, key);
+            }
+        }
+    }
+ 
+};
