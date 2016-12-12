b2ee8a43d266809e1eb343c4c186dbdb22ce4c18
LiliMeng <lilimeng1103@gmail.com>
2016-12-12 09:23:25 -0800

    Create 285_Inorder_Successor_in_BST.cpp

---
+++ 285_Inorder_Successor_in_BST.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+        
+        stack<TreeNode*> s;
+        bool b = false;
+        TreeNode *t = root;
+        while (t || !s.empty()) {
+            while (t) {
+                s.push(t);
+                t = t->left;
+            }
+            t = s.top(); s.pop();
+            if (b) return t;
+            if (t == p) b = true;
+            t = t->right;
+        }
+        return NULL;
+    }
+};
